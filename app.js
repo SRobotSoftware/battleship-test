af3c08d011d0507db0955da72f16380ae1a0a662
Zmixon91 <zmixon91@gmail.com>
2016-01-25 12:40:15 -0700

    fixed validation for larger grids

--- app.js
+++ app.js
@@ -92,17 +92,17 @@ function validate(ships) {
         return all;
     }, []);
     // Prep arr for validation
-    var validate = shipCoords.sort(function (a, b) { return b - a })
+    var validateMe = shipCoords.sort(function (a, b) { return b - a })
+    console.log(shipCoords);
     // check for duplicates
-    validate.reduce(function (all, item, index) {
+    validateMe.reduce(function (all, item, index) {
         var nextIndex = index + 1;
-        if (item === validate[nextIndex]) err = true;
+        if (item === validateMe[nextIndex]) err = true;
         return all;
     }, 0);
     // constrain to grid
-    if (validate[validate.length - 1] < 0) err = true;
-    for (var i in validate) {
-        if (validate[i].length > 2) err = true;
+    for (var i in validateMe) {
+        if (validateMe[i] > '' + (grid.A.length - 1) + (grid.A.length - 1)) err = true;
     }
     return err;
 }
@@ -120,15 +120,16 @@ function hideShips(grid, shipList) {
         // place initial ship part randomly
         currentShip.onGrid[0] = { row: myRow, col: myCol };
         // place other ship parts until ship is complete
+        // THIS NEEDS CLEANUP
         for (var j = 1; j < currentShip.size; j++) {
             if (orientation === 1 && direction === 1) {
-                currentShip.onGrid[j] = { row: currentShip.onGrid[0].row - j, col: currentShip.onGrid[0].col };
+                currentShip.onGrid[j] = { row: Math.max(0, Math.min(currentShip.onGrid[0].row - j, gridSize - 1)), col: currentShip.onGrid[0].col };
             } else if (orientation === 1 && direction === 0) {
-                currentShip.onGrid[j] = { row: currentShip.onGrid[0].row + j, col: currentShip.onGrid[0].col };
+                currentShip.onGrid[j] = { row: Math.max(0, Math.min(currentShip.onGrid[0].row + j, gridSize - 1)), col: currentShip.onGrid[0].col };
             } else if (orientation === 0 && direction === 1) {
-                currentShip.onGrid[j] = { row: currentShip.onGrid[0].row, col: currentShip.onGrid[0].col + j };
+                currentShip.onGrid[j] = { row: currentShip.onGrid[0].row, col: Math.max(0, Math.min(currentShip.onGrid[0].col + j, gridSize - 1)) };
             } else if (orientation === 0 && direction === 0) {
-                currentShip.onGrid[j] = { row: currentShip.onGrid[0].row, col: currentShip.onGrid[0].col - j };
+                currentShip.onGrid[j] = { row: currentShip.onGrid[0].row, col: Math.max(0, Math.min(currentShip.onGrid[0].col - j, gridSize - 1)) };
             }
         }
     });
@@ -141,15 +142,16 @@ function drawGrid(id, grid, ships) {
     for (var row in grid) {
         $("#" + id).find('tbody').append($('<tr>'));
         grid[row].forEach(function (col) {
+            var myCoords = col.name.split("");
+            var myRow = rows.indexOf(myCoords[0]);
+            var myCol = col.name.match(/(\d+)$/g);;
             ships.forEach(function (ship) {
                 ship.onGrid.forEach(function (coord) {
-                    var myCoords = col.name.split("");
-                    var myRow = rows.indexOf(myCoords[0]);
-                    if ('' + coord.row + coord.col === '' + myRow + myCoords[1]) { flag = true; }
+                    if ('' + coord.row + coord.col === '' + myRow + myCol) { flag = true; }
                 });
             });
             // Please note that this is simply placeholder to make it functional, will eventually use classes instead of styles
-            (flag) ? $("#" + id).find('tbody').append($('<td>').append($('<p>').text('S').attr('style', 'background-color: green;'))) : $("#" + id).find('tbody').append($('<td>').append($('<p>').text('W').attr('style', 'background-color: blue;')));
+            (flag) ? $("#" + id).find('tbody').append($('<td>').append($('<p>').text(myRow+" "+myCol).attr('style', 'background-color: green;'))) : $("#" + id).find('tbody').append($('<td>').append($('<p>').text(myRow+" "+myCol).attr('style', 'background-color: blue;')));
             flag = false;
         });
 
@@ -164,13 +166,16 @@ var shipyard = [
     { name: "patrol", size: 2, onGrid: [], hits: 0 }
 ];
 // Program vars
-var ships = shipyard//createShips(5, 3);
+var ships = shipyard;//createShips(20, 5);
+// 10x10
 var rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+// 26x26
+// var rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var grid = createGrid(rows);
 var test = hideShips(grid, ships);
 // Game logic
 // Attempt to hide ships x times before giving up
-var attemptLimit = 20;
+var attemptLimit = 100;
 var attempts = 0;
 do {
     test = hideShips(grid, ships);
